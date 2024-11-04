5135e36091502bf41aa071cfb6c56ecd10f637fb
Mikael Ronström <mikael.ronstrom@gmail.com>
2024-11-04 17:29:39 +0100

    RONDB-756: Added include paths for Google Protobuf on Mac

--- pink/build.sh
+++ pink/build.sh
@@ -15,7 +15,11 @@ if [[ ! -d $SLASH_PATH ]]; then
   git clone https://github.com/Qihoo360/slash.git $SLASH_PATH
 fi
 cd $SLASH_PATH/slash && make
-
+UNAME_S=`uname -s`
+if test "x$UNAME_S" = "xDarwin" ; then
+  export C_INCLUDE_PATH=/opt/homebrew_include
+  export CPLUS_INCLUDE_PATH=/opt/homebrew/include
+fi
 # Compile pink
 cd $PINK_PATH
 make SLASH_PATH=$SLASH_PATH
