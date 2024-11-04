aba86dd52b01d0813884a86bdb4ba158b5986e89
Mikael Ronström <mikael.ronstrom@gmail.com>
2024-11-04 19:06:55 +0100

    RONDB-756: Review comment handling

--- pink/build.sh
+++ pink/build.sh
@@ -3,17 +3,7 @@
 set -e
 
 PINK_PATH=$PWD
-
-# We depend on slash
-SLASH_PATH=$1
-if test -z $SLASH_PATH; then
-  SLASH_PATH=$PINK_PATH/third/slash
-fi
-
-if [[ ! -d $SLASH_PATH ]]; then
-  echo "Slash library is not available"
-  exit 1
-fi
+SLASH_PATH=$PINK_PATH/third/slash
 cd $SLASH_PATH/slash && make
 UNAME_S=`uname -s`
 if test "x$UNAME_S" = "xDarwin" ; then
@@ -23,4 +13,3 @@ fi
 # Compile pink
 cd $PINK_PATH
 make SLASH_PATH=$SLASH_PATH
-cd rondis && make SLASH_PATH=$SLASH_PATH
